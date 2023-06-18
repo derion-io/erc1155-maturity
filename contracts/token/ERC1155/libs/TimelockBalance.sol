108be9fa38e08cee8ec466d90d3ccf86da29a23c
Zergity <zergity@gmail.com>
2023-06-18 18:11:17 +0700

    constants for MASK and MAX

--- contracts/token/ERC1155/libs/TimelockBalance.sol
+++ contracts/token/ERC1155/libs/TimelockBalance.sol
@@ -4,18 +4,20 @@ pragma solidity ^0.8.0;
 import "./SimpleMath.sol";
 
 library TimelockBalance {
-    uint constant MAX_TIME = uint(type(uint32).max) << 224;
-    uint constant MAX_BALANCE = type(uint224).max;
+    uint constant TIME_MAX = type(uint32).max;
+    uint constant TIME_MASK = TIME_MAX << 224;
+    uint constant BALANCE_MAX = type(uint224).max;
+    uint constant BALANCE_MASK = BALANCE_MAX;
 
     function pack(uint b, uint t) internal pure returns (uint) {
         require(t <= type(uint32).max, "Timelock: t overflow");
-        require(b <= MAX_BALANCE, "Timelock: b overflow");
+        require(b <= BALANCE_MAX, "Timelock: b overflow");
         return (t << 224) | b;
     }
 
     function amount(uint x) internal pure returns (uint) {
         unchecked {
-            return x & MAX_BALANCE;
+            return x & BALANCE_MASK;
         }
     }
 
@@ -33,23 +35,23 @@ library TimelockBalance {
             uint xt = SimpleMath.max(block.timestamp, x >> 224);
             uint yt = y >> 224;
             require(yt <= xt, "Timelock: locktime order");
-            uint yb = y & MAX_BALANCE;
-            uint xb = x & MAX_BALANCE;
+            uint yb = y & BALANCE_MASK;
+            uint xb = x & BALANCE_MASK;
             uint zb = xb + yb;
-            require(zb <= MAX_BALANCE, "Timelock: zb overflow");
+            require(zb <= BALANCE_MAX, "Timelock: zb overflow");
             return x + yb;
         }
     }
 
     function split(uint z, uint yb) internal pure returns (uint x, uint y) {
         unchecked {
-            uint zb = z & MAX_BALANCE;
+            uint zb = z & BALANCE_MASK;
             if (zb == yb) {
                 return (0, z); // full transfer
             }
             require(zb > yb, "Timelock: insufficient balance");
             x = z - yb; // preserve the locktime
-            y = (z & MAX_TIME) | yb;
+            y = (z & TIME_MASK) | yb;
         }
     }
 }
